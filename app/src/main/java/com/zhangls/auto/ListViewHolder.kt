99d6a4cd2558250cf0aad4143a85b117993fb915
zhangls <zhangls2014@gmail.com>
2018-05-26 22:25:47 +0800

    update gradle

--- app/src/main/java/com/zhangls/auto/ListViewHolder.kt
+++ app/src/main/java/com/zhangls/auto/ListViewHolder.kt
@@ -7,12 +7,11 @@ import android.widget.TextView
 import org.jetbrains.anko.find
 
 /**
- * ViewHolder 子类
+ * ViewHolder 子类.
  *
  * @author zhangls
  */
 class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
     val title: TextView = itemView.find(R.id.tvOperator)
     val upload: TextView = itemView.find(R.id.tvUpload)
     val time: TextView = itemView.find(R.id.tvTime)
